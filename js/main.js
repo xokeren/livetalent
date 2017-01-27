502dfcced756805daaea45c8f452dea7c75d60c9
xokeren <kerenf@sfu.ca>
2017-01-27 15:33:35 -0800

    playing around with settings

--- js/main.js
+++ js/main.js
@@ -1,49 +1,130 @@
-jQuery(document).ready(function($){
-	var contentSections = $('.cd-section'),
-		navigationItems = $('#cd-vertical-nav a');
+jQuery(document).ready(function(){
+	//cache DOM elements
+	var mainContent = $('.cd-main-content'),
+		header = $('.cd-main-header'),
+		sidebar = $('.cd-side-nav'),
+		sidebarTrigger = $('.cd-nav-trigger'),
+		topNavigation = $('.cd-top-nav'),
+		searchForm = $('.cd-search'),
+		accountInfo = $('.account');
 
-	updateNavigation();
-	$(window).on('scroll', function(){
-		updateNavigation();
+	//on resize, move search and top nav position according to window width
+	var resizing = false;
+	moveNavigation();
+	$(window).on('resize', function(){
+		if( !resizing ) {
+			(!window.requestAnimationFrame) ? setTimeout(moveNavigation, 300) : window.requestAnimationFrame(moveNavigation);
+			resizing = true;
+		}
 	});
 
-	//smooth scroll to the section
-	navigationItems.on('click', function(event){
-        event.preventDefault();
-        smoothScroll($(this.hash));
-    });
-    //smooth scroll to second section
-    $('.cd-scroll-down').on('click', function(event){
-        event.preventDefault();
-        smoothScroll($(this.hash));
-    });
+	//on window scrolling - fix sidebar nav
+	var scrolling = false;
+	checkScrollbarPosition();
+	$(window).on('scroll', function(){
+		if( !scrolling ) {
+			(!window.requestAnimationFrame) ? setTimeout(checkScrollbarPosition, 300) : window.requestAnimationFrame(checkScrollbarPosition);
+			scrolling = true;
+		}
+	});
 
-    //open-close navigation on touch devices
-    $('.touch .cd-nav-trigger').on('click', function(){
-    	$('.touch #cd-vertical-nav').toggleClass('open');
-  
-    });
-    //close navigation on touch devices when selectin an elemnt from the list
-    $('.touch #cd-vertical-nav a').on('click', function(){
-    	$('.touch #cd-vertical-nav').removeClass('open');
-    });
+	//mobile only - open sidebar when user clicks the hamburger menu
+	sidebarTrigger.on('click', function(event){
+		event.preventDefault();
+		$([sidebar, sidebarTrigger]).toggleClass('nav-is-visible');
+	});
 
-	function updateNavigation() {
-		contentSections.each(function(){
-			$this = $(this);
-			var activeSection = $('#cd-vertical-nav a[href="#'+$this.attr('id')+'"]').data('number') - 1;
-			if ( ( $this.offset().top - $(window).height()/2 < $(window).scrollTop() ) && ( $this.offset().top + $this.height() - $(window).height()/2 > $(window).scrollTop() ) ) {
-				navigationItems.eq(activeSection).addClass('is-selected');
-			}else {
-				navigationItems.eq(activeSection).removeClass('is-selected');
+	//click on item and show submenu
+	$('.has-children > a').on('click', function(event){
+		var mq = checkMQ(),
+			selectedItem = $(this);
+		if( mq == 'mobile' || mq == 'tablet' ) {
+			event.preventDefault();
+			if( selectedItem.parent('li').hasClass('selected')) {
+				selectedItem.parent('li').removeClass('selected');
+			} else {
+				sidebar.find('.has-children.selected').removeClass('selected');
+				accountInfo.removeClass('selected');
+				selectedItem.parent('li').addClass('selected');
 			}
-		});
+		}
+	});
+
+	//click on account and show submenu - desktop version only
+	accountInfo.children('a').on('click', function(event){
+		var mq = checkMQ(),
+			selectedItem = $(this);
+		if( mq == 'desktop') {
+			event.preventDefault();
+			accountInfo.toggleClass('selected');
+			sidebar.find('.has-children.selected').removeClass('selected');
+		}
+	});
+
+	$(document).on('click', function(event){
+		if( !$(event.target).is('.has-children a') ) {
+			sidebar.find('.has-children.selected').removeClass('selected');
+			accountInfo.removeClass('selected');
+		}
+	});
+
+	//on desktop - differentiate between a user trying to hover over a dropdown item vs trying to navigate into a submenu's contents
+	sidebar.children('ul').menuAim({
+        activate: function(row) {
+        	$(row).addClass('hover');
+        },
+        deactivate: function(row) {
+        	$(row).removeClass('hover');
+        },
+        exitMenu: function() {
+        	sidebar.find('.hover').removeClass('hover');
+        	return true;
+        },
+        submenuSelector: ".has-children",
+    });
+
+	function checkMQ() {
+		//check if mobile or desktop device
+		return window.getComputedStyle(document.querySelector('.cd-main-content'), '::before').getPropertyValue('content').replace(/'/g, "").replace(/"/g, "");
 	}
 
-	function smoothScroll(target) {
-        $('body,html').animate(
-        	{'scrollTop':target.offset().top},
-        	600
-        );
+	function moveNavigation(){
+  		var mq = checkMQ();
+        
+        if ( mq == 'mobile' && topNavigation.parents('.cd-side-nav').length == 0 ) {
+        	detachElements();
+			topNavigation.appendTo(sidebar);
+			searchForm.removeClass('is-hidden').prependTo(sidebar);
+		} else if ( ( mq == 'tablet' || mq == 'desktop') &&  topNavigation.parents('.cd-side-nav').length > 0 ) {
+			detachElements();
+			searchForm.insertAfter(header.find('.cd-logo'));
+			topNavigation.appendTo(header.find('.cd-nav'));
+		}
+		checkSelected(mq);
+		resizing = false;
+	}
+
+	function detachElements() {
+		topNavigation.detach();
+		searchForm.detach();
+	}
+
+	function checkSelected(mq) {
+		//on desktop, remove selected class from items selected on mobile/tablet version
+		if( mq == 'desktop' ) $('.has-children.selected').removeClass('selected');
+	}
+
+	function checkScrollbarPosition() {
+		var mq = checkMQ();
+		
+		if( mq != 'mobile' ) {
+			var sidebarHeight = sidebar.outerHeight(),
+				windowHeight = $(window).height(),
+				mainContentHeight = mainContent.outerHeight(),
+				scrollTop = $(window).scrollTop();
+
+			( ( scrollTop + windowHeight > sidebarHeight ) && ( mainContentHeight - sidebarHeight != 0 ) ) ? sidebar.addClass('is-fixed').css('bottom', 0) : sidebar.removeClass('is-fixed').attr('style', '');
+		}
+		scrolling = false;
 	}
 });
